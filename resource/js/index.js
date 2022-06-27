7d50d706a869390e6446c966060b34011fc60560
chkue2 <chkue0924@gmail.com>
2022-06-27 10:32:16 +0900

    pc complete

--- resource/js/index.js
+++ resource/js/index.js
@@ -1,3 +1,5 @@
+let swiper2 = null
+let swiper3 = null
 $(document).ready(function(){
   const swiper1 = new Swiper('.carton', {
     centeredSlides: true,
@@ -13,4 +15,44 @@ $(document).ready(function(){
       prevEl: '.carton .button.prev',
     },
   });
+  swiper2 = new Swiper('.funding .left-swiper', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 11,
+    loop: true,
+    loopedSlides: 5,  
+    autoHeight: false,
+    observer: true,
+    observeParents: true,
+    touchRatio: 0
+  });
+  swiper3 = new Swiper('.funding .right-swiper', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 50,
+    loop: true,
+    loopedSlides: 8,  
+    autoHeight: true,
+    observer: true,
+    observeParents: true,
+    on:{
+      slidePrevTransitionStart(){
+        swiper2.slidePrev()
+      },
+      slideNextTransitionStart(){
+        swiper2.slideNext()
+      }
+    }
+  });
+})
+
+$(document).on('click', '.news .list .item', function(){
+  const target = $(this).children('.top').attr('class')
+  if(target.includes('on')){
+    $(this).children('.top').removeClass('on')
+  } else {
+    $(this).children('.top').addClass('on')
+  }
 })
